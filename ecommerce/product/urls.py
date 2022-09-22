b410f1e56a19e0aeb33c14054120e014a98016e8
samirvithlani <samir.vithlani83955@gmail.com>
2022-09-22 12:26:24 +0530

    crud

--- ecommerce/product/urls.py
+++ ecommerce/product/urls.py
@@ -5,7 +5,10 @@ from . import views
 urlpatterns = [
 
 path('products/',views.products),
-path('register/',views.RegisterProductView.as_view()),
+path('register/',views.RegisterProductView.as_view(),name="register"),
 path('list/',views.ListProductView.as_view()),
+path('delete/<int:pk>',views.DeleteProductView.as_view(),name="delete_product"),
+path('productdetail/<int:pk>',views.DetailProductView.as_view(),name="product_detail"),
+path('update/<int:pk>',views.UpdateProductView.as_view(),name="update_product"),
     
 ]
--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-from django.views.generic.edit import CreateView
-from django.views.generic import ListView
+from django.views.generic.edit import CreateView,DeleteView,UpdateView
+from django.views.generic import ListView,DetailView
 
 from .models import Product
 
@@ -14,10 +14,26 @@ class RegisterProductView(CreateView):
     model  = Product
     fields = "__all__"
     template_name = "products/product_register.html"
-    success_ur ="/"
+    success_url = "/product/list/"
     
 class ListProductView(ListView):
     model = Product
     template_name = "products/product_list.html"
     context_object_name = 'products'    
+
+class DeleteProductView(DeleteView):
+    model = Product
+    template_name = "products/product_delete.html"
+    success_url = "/product/list/"   
     
+class DetailProductView(DetailView):
+    model = Product
+    template_name = "products/product_detail.html"
+    context_object_name = "product"  
+
+class UpdateProductView(UpdateView):
+    model = Product
+    template_name = "products/product_update.html"
+    context_object_name = "product"
+    fields = "__all__"
+    success_url = "/product/list/"
